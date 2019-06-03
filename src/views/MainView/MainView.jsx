72cd1b86f62423468a10cf325c3308fd3d31b490
Kuba <ja.tanski@gmail.com>
2019-06-03 13:49:55 +0200

    <feat> add loguot

--- src/views/MainView/MainView.jsx
+++ src/views/MainView/MainView.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./mainView.scss";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class MainView extends Component {
+  deleteToken() {
+    localStorage.removeItem("x-auth-token");
+  }
   render() {
     return (
       <section className="mainView">
         <p>Zalogowałeś się pomyślnie.</p>
         <p>Tutaj jest główne Menu</p>
+        <Button variant="warning" onClick={this.deleteToken}>
+          <Link to="/">Wyloguj się!</Link>
+        </Button>
       </section>
     );
   }
