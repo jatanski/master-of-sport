a0dbc04f30aadc5b085fa6b20ff27b9650265468
Kuba <ja.tanski@gmail.com>
2019-06-10 15:40:05 +0200

    <feat> add bmi GET route

--- server/routes/bmi.js
+++ server/routes/bmi.js
@@ -8,7 +8,6 @@ const router = express.Router();
 router.post("/", auth, async (req, res) => {
   let user;
   user = await User.findById(req.user);
-  if (!user) return res.status(400).send("Invalid email or password.");
 
   //create new bmi object
   console.log(user);
@@ -19,6 +18,13 @@ router.post("/", auth, async (req, res) => {
 
   user.statistics.bmi.push(bmi);
   await user.save();
+
+  res.status(200).send(user.statistics.bmi);
+});
+
+router.get("/", auth, async (res, req) => {
+  let user;
+  user = await User.findById(req.user);
 
   res.status(200).send(user.statistics.bmi);
 });
--- server/routes/statistics.js
+++ server/routes/statistics.js
@@ -1,11 +1,6 @@
 import express from "express";
 import Statistics from "../models/statisticsModel";
-import auth from '../middleware/auth'
+import auth from "../middleware/auth";
+import { User } from "../models/userModel";
 
 const router = express.Router();
-
-router.post("/",auth, async (req, res) => {
-  const { value, date } = req.body;
-
-  let bmi = await 
-});
