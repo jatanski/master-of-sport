3882e62a9089c7f921c00de1b1445a38d6f8c685
Kuba <ja.tanski@gmail.com>
2019-06-10 15:32:37 +0200

    <feat> add [BmiStatistics]

--- src/views/Statistics/Statistics.jsx
+++ src/views/Statistics/Statistics.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import "./statistics.scss";
+import { Button } from "react-bootstrap";
+import BmiStatistics from "../../components/BmiStatistics/BmiStatistics";
 
 export default class Statistics extends Component {
+  state = {
+    showBmi: false,
+    showCircuits: false
+  };
+  showBmiTable = () => {
+    this.setState({ showBmi: true });
+  };
+  showCircuitsTable = () => {};
+
   render() {
     return (
-      <div>
-        <p>Tutaj będą statystyki.</p>
-      </div>
+      <section className="statistics">
+        <Button onClick={this.showBmiTable} size="lg" variant="primary">
+          BMI
+        </Button>
+        <Button onClick={this.showCircuitsTable} size="lg" variant="primary">
+          Obwody i waga
+        </Button>
+        {this.state.showBmi ? <BmiStatistics /> : null}
+      </section>
     );
   }
 }
